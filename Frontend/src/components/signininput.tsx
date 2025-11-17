66bd9af0f8f0c9a1ea3e359840a8bf0f28a89a02
Utkarsh Wadbudhe <119593648+Utkarsh5356@users.noreply.github.com>
2025-11-18 03:40:21 +0530

    dd

--- Frontend/src/components/signininput.tsx
+++ Frontend/src/components/signininput.tsx
@@ -5,11 +5,10 @@ import {InputOTP,InputOTPGroup,InputOTPSlot} from "./ui/input-otp"
 import Label from "./ui/label"
 import { Link,useNavigate } from "react-router"
 import { useState } from "react"
-import { useSignIn,useUser } from "@clerk/react-router"
+import { useSignIn } from "@clerk/react-router"
 
 export default function SignInInput(){
   const {isLoaded,setActive,signIn}=useSignIn()
-  // const {isSignedIn,user}=useUser()
   const [email,setEmail]=useState("")
   const [error,setError]=useState("")
   const [code,setCode]=useState("")
@@ -25,22 +24,13 @@ export default function SignInInput(){
       strategy:"email_code", 
       identifier:email,
      })
-    //  if(isSignedIn){
-    //  await user.reload()
-    //  const emailid=user.emailAddresses.find((e)=>e.emailAddress === email)
-    //  await signIn.prepareFirstFactor({
-    //   // emailAddressId:emailid?.id ?? user.primaryEmailAddressId,
-    //   strategy:"email_code",
-    //  })
-    //  }
-     
      setPendingVerification(true)
-    }catch(err:any){
-    console.log(err)
-    setError(err.errors[0].message)
-    } 
-  }
-
+     }catch(err:any){
+     console.log(err)
+     setError(err.errors[0].message)
+    }
+  } 
+ 
   async function onPressVerify(){
     if(!isLoaded) return <>Loading...</>
     try{
@@ -64,7 +54,20 @@ export default function SignInInput(){
     }
   }
 
-    return (
+  async function googleSignIn(){
+    if(!isLoaded) return <>Loading...</>
+    try{
+     await signIn.authenticateWithRedirect({
+      strategy:"oauth_google",
+      redirectUrl:"/sso-callback",
+      redirectUrlComplete:"/channels"
+    })
+    }catch(err:any){
+     console.log(err)
+    }
+  }
+  
+  return (
      <Card className="w-full max-w-sm">
       {!pendingVerification ? 
       <>
@@ -75,7 +78,7 @@ export default function SignInInput(){
           We're excited to see you again!
        </div>
        <div className="flex justify-center mt-2 "> 
-          <Button variant="outline" className="bg-white text-gray-600 text-sm font-semibold gap-4 px-20 h-8 ">
+          <Button onClick={googleSignIn} variant="outline" className="bg-white text-gray-600 text-sm font-semibold gap-4 px-20 h-8 ">
             <span>
               <svg width="64px" height="64px" viewBox="-3 0 262 262" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" fill="#4285F4"></path><path d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" fill="#34A853"></path><path d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" fill="#FBBC05"></path><path d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" fill="#EB4335"></path></g></svg>
             </span> Continue with Google
